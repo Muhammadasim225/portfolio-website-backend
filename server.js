3502b64f295ddc8c7ec27d21a943ebbc5e758ddd
Hi 👋, I'm Mohammad Asim <muhammadasim4927@gmail.com>
2025-05-29 18:48:21 +0500

    Initial commit of backend code

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const dotenv=require('dotenv').config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(cors({
+  origin: 'https://your-frontend.vercel.app', // allow your frontend domain
+  methods: ['POST', 'GET'],
+  credentials: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// POST route to handle form submissions
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Configure nodemailer to use Gmail SMTP
+  const transporter = nodemailer.createTransport({
+    service: "gmail", // Correct service field
+    host:"smtp.gmail.com",
+    port:465,
+    secure: true, // Use SSL
+
+    auth: {
+      user: process.env.EMAIL, // Your Gmail email address
+      pass: process.env.APP_PASSWORD, 
+    },
+  });
+
+  // Mail options
+  const mailOptions = {
+    from: email,
+    to: process.env.EMAIL, // Where you want to receive the emails
+    subject: `New message from ${name}`,
+    text: `You have a new message from:
+           Name: ${name}
+           Email: ${email}
+           Message: ${message}`,
+  };
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send("Failed to send email");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
